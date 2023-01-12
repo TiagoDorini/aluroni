1288edc68fc5c8ed89483c29118e53f6734b95d9
Tiago Dorini <td.dorini@gmail.com>
2023-01-11 21:41:48 -0300

    feat: add go back btn to 404 page

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -2,8 +2,11 @@ import styles from './NotFound.module.scss'
 import { ReactComponent as NotFoundImage } from 'assets/not_found.svg'
 import classNames from 'classnames'
 import theme from 'styles/Theme.module.scss'
+import { useNavigate } from 'react-router-dom'
 
 export default function NotFound() {
+	const navigate = useNavigate()
+
 	return (
 		<div
 			className={classNames({
@@ -11,6 +14,9 @@ export default function NotFound() {
 				[theme.container]: true,
 			})}
 		>
+			<div className={styles.return}>
+				<button onClick={() => navigate(-1)}>{'< Go Back'}</button>
+			</div>
 			<NotFoundImage />
 		</div>
 	)
--- src/routes.jsx
+++ src/routes.jsx
@@ -9,7 +9,7 @@ import NotFound from 'pages/NotFound'
 
 export default function AppRouter() {
 	return (
-		<main>
+		<main className='container'>
 			<Router>
 				<Menu />
 				<Routes>
